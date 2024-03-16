b9b9148c15bbf376358c89b29bb2c553dc99b57b
Bikoo <bikoo3002@naver.com>
2024-03-16 22:30:03 +0900

    게임 시작 입력 폼 작성

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-  /** Initialize MDC Web components. */
-  const buttons = document.querySelectorAll('.mdc-button');
-  for (const button of buttons) {
+/** Initialize MDC Web components. */
+const buttons = document.querySelectorAll('.mdc-button');
+for (const button of buttons) {
     mdc.ripple.MDCRipple.attachTo(button);
-  }
-  
-  const textFields = document.querySelectorAll('.mdc-text-field');
-  for (const textField of textFields) {
+}
+
+const textFields = document.querySelectorAll('.mdc-text-field');
+for (const textField of textFields) {
     mdc.textField.MDCTextField.attachTo(textField);
-  }
-  
-  /** Custom javascript code. */
-  const greetMessageEl = document.querySelector('.greet-message');
-  const greetButton = document.querySelector('.greet-button');
-  greetButton.addEventListener('click', () => {
+}
+
+/** Custom javascript code. */
+const greetMessageEl = document.querySelector('.greet-message');
+const greetButton = document.querySelector('.greet-button');
+greetButton.addEventListener('click', () => {
     const firstNameInput = document.querySelector('.first-name-input').value;
     const lastNameInput = document.querySelector('.last-name-input').value;
     let name;
     if (firstNameInput || lastNameInput) {
-      name = firstNameInput + ' ' + lastNameInput;
+        name = firstNameInput + ' ' + lastNameInput;
     } else {
-      name = 'Anonymous';
+        name = 'Anonymous';
     }
     greetMessageEl.textContent = `Hello, ${name}!`;
-  });
+});
+
+
+const dialog = new mdc.dialog.MDCDialog(document.querySelector('.mdc-dialog'));
+const showDialogButton = document.querySelector('#show-dialog');
+showDialogButton.addEventListener('click', function () {
+    dialog.open();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dialog = new mdc.dialog.MDCDialog(document.querySelector('.mdc-dialog'));
+    dialog.open();
+});
