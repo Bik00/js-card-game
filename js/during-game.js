792ad9b7877dd723addf11bb6d602e24ecc0bc60
Bikoo <bikoo3002@naver.com>
2024-03-19 17:33:23 +0900

    게임 규칙 - 2

--- js/during-game.js
+++ js/during-game.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     $(document).on('click', '.card', function () {
         const $values = $('.values');
-        if ($values.hasClass("game-started") && pauseFlipping == false) {
+        if ($values.hasClass("game-started") && pauseFlipping == false && !$values.hasClass("corrected")) {
 
             checkCard($(this));
         }
@@ -14,7 +14,17 @@ $(document).ready(function() {
 });
 
 function checkCard($card) {
-    if ($card.css('transform') != "none" && flippedCards.length < 2) {
+
+    // 카드가 이미 뒤집히는 중인지 확인
+    if ($card.hasClass('flipping')) {
+        // 뒤집히는 중이면 클릭 무시
+        return;
+    }
+
+    // 뒤집히는 중 플래그 추가
+    $card.addClass('flipping');
+
+    if ($card.css('transform') != "none" && flippedCards.length < 2 && !flippedCards.includes($card)) {
         $card.css('transform', '');
         flippedCards.push($card);
 
@@ -23,20 +33,30 @@ function checkCard($card) {
             pauseFlipping = true; // 더 이상의 카드 뒤집기를 잠시 멈춤
             if (flippedCards[0].data('value') === flippedCards[1].data('value')) {
                 // 카드가 일치하는 경우 처리
+                let score = $(".board_score").find("span").text();
+                $(".board_score").find("span").text(parseInt(score) + 30 + "");
+                flippedCards.forEach(function(card) {
+                    card.addClass("corrected");
+                });                
                 flippedCards = []; // 배열 초기화
                 pauseFlipping = false; // 카드 뒤집기 재개
             } else {
                 // 카드가 일치하지 않는 경우, 1초 후에 다시 뒤집음
                 setTimeout(() => {
-                    flippedCards.forEach(card => {
-                        card.css('transform', 'rotateY(180deg)'); // 카드를 원래 상태로 되돌림
-                    });
+                    flippedCards.forEach(function(card) {
+                        card.css('transform', 'rotateY(180deg)');
+                    });    
                     flippedCards = []; // 배열 초기화
                     pauseFlipping = false; // 카드 뒤집기 재개
                 }, 1000);
             }
         }
     }
+
+    // 모든 처리가 끝난 후에 'flipping' 클래스를 제거하여 다시 클릭 가능하게 함
+    setTimeout(() => {
+        $card.removeClass('flipping');
+    }, 500); // 예: 500ms 후에 처리, 카드 뒤집기 애니메이션 시간에 맞춰 조정 필요
 }
 
 
